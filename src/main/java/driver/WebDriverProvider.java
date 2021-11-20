31843ba60dc09369b0b03e40a77c979b81a8d60a
Тимур <tvborodich@bk.ru>
2021-11-20 20:59:37 +0300

    Initial commit

---
+++ src/main/java/driver/WebDriverProvider.java
@@ -0,0 +1,30 @@
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverProvider {
+    private static WebDriver driver;
+
+    private WebDriverProvider() {
+    }
+
+    public static WebDriver getDriver() {
+        if (null == driver) {
+            switch (System.getProperty("browser")) {
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+            }
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.quit();
+        driver = null;
+    }
+}
---
+++ src/test/java/util.java
@@ -0,0 +1,6 @@
+import org.testng.annotations.Listeners;
+import util.TestListeners;
+
+@Listeners({TestListeners.class})
+public class util {
+}
